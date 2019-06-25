9da4e34407e15098d581092ecb27f23307beaa9e
guiilhermerod <49196332+guiilhermerod@users.noreply.github.com>
2019-06-25 18:01:45 -0300

    Update atv2.py

--- atv2.py
+++ atv2.py
@@ -1,33 +1,40 @@
 def valor(x):
     x= str(x)
     l= str.__len__(x)
+    v=','
     if l==1:
         print (x[0])
     elif l==2:
         print(x[0:2])
     elif l==3:
         print (x[0:3])
-    elif l>4 and l<=6:
+    elif l==4:
         msg= "mil"
-        v= ','
+        print(x[0],v,x[1:3],msg)
+    elif l==5:
+        msg= 'mil'
+        print(x[0:2],v,x[2:4],msg)
+    elif l==6:
+        msg= "mil"
+        print (x[0:3],v,x[1:3],msg)
+    elif l==7:
+        msg="milhões"
         print (x[0],v,x[1:3],msg)
-    elif l>=7 and l<=9 and x[0]=="1":
-        v= ','
-        msg="milhão"
-        print (x[0],v,x[1:3],msg)
-    elif l>=7 and l<=9 and x[0] !="1":
-        v= ','
-        msg= "milhões"
-        print (x[0],v,x[1:3],msg)
-    elif l>=10 and l<=12 and x[0] == "1":
-        v= ','
+    elif l==8:
+        msg="milhões"
+        print (x[0:2],v,x[1:3],msg)
+    elif l==9:
+        msg="milhões"
+        print (x[0:3],v,x[2:4],msg)
+    elif l==10:
         msg= "bilhão"
         print (x[0],v,x[1:3],msg)
-    elif l>=10 and l<=12 and x[0]!= "1":
-        v= ','
+    elif l==11:
         msg= "bilhões"
-        print (x[0],v,x[1:3],msg)
-        
-valor(312111)
-    
+        print (x[0:2],v,x[1:3],msg)
+    elif l==12:
+        msg="bilhões"
+        print(x[0:3],v,x[2:4],msg)
 
+valor("INSIRA O VALOR AQUI")
+    
